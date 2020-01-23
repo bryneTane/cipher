b6074f7a5b9d43189a3f0286b39114976d8fc9da
TANE TSAMO Friedrich Bryne <bftane@gmail.com>
2020-01-23 12:16:55 +0100

    alert

--- routes/methods.js
+++ routes/methods.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET methods page. */
 router.get('/', function (req, res, next) {
-    res.render('methods', { title: 'Express' });
+    res.render('methods', { title: 'Methods' });
 });
 
 
