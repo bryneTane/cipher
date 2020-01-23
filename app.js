70bb8b0958ee15c7c01167d6af37a38d8f6886d2
Wilfried <wilfried.djopa@gmail.com>
2020-01-23 11:52:55 +0100

    Ajout des autres pages

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var app = express();
-global.file = null;
+global.datas = {};
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-
+const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({
+  extended: true
+}));
 var storage = multer.diskStorage({
   destination: function (req, file, callback) {
     callback(null, './uploads');
@@ -14,11 +17,42 @@ var storage = multer.diskStorage({
 var upload = multer({
   storage: storage
 }).single('file');
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Home'
+  });
 });
+/* GET password page. */
+router.get('/password', function (req, res, next) {
+  res.render('password', {
+    title: 'Password'
+  });
+});
+/* GET download page. */
+router.get('/download', function (req, res, next) {
+  if (global.datas.password == undefined) {
+        res.redirect("password")
+
+  }
+  res.render('download', {
+    title: 'Download'
+  });
+});
+
+
+router.post("/password", (req, res)=>{
+    global.datas.password = req.body.password
+    res.redirect("download")
+})
+
+router.get("/hashfile", (req, res) => {
+  if(global.datas.file == undefined){
+    res.redirect("/")
+  }
+  console.log(global.datas.file)
+  //res.download(global.datas.file)
+})
 
 router.post('/file', function (req, res) {
   upload(req, res, function (err) {
@@ -27,7 +61,7 @@ router.post('/file', function (req, res) {
       return res.end("Error uploading file.");
     }
     console.log(req.file)
-    global.file  = req.file
+    global.datas.file  = req.file
     res.send("success");
   });
 });
